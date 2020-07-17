28c30f0c8ddc38b35be67576593768a5d1634374
fehepe <40445768+fehepe@users.noreply.github.com>
2020-07-17 00:17:56 -0400

    LocalidadService aun faltante la busquedad de los barrios por municipio

--- Proyects/Xamarin/CitizenApp/CitizenApp/CitizenApp/Services/DataStores/LocalidadService.cs
+++ Proyects/Xamarin/CitizenApp/CitizenApp/CitizenApp/Services/DataStores/LocalidadService.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace CitizenApp.Services.DataStores
 {
@@ -77,5 +78,67 @@ namespace CitizenApp.Services.DataStores
                 new Barrio{BarrioId=19, SectorId=4,Nombre = "La Carmelita" },
             };
         }
+
+        public async Task<List<Barrio>> ObtenerBarriosPorSectorID(int sectorId)
+        {
+            var barriosResult = barrios.FindAll(x => x.SectorId == sectorId);
+            return await Task.FromResult(barriosResult);
+        }
+
+        public async Task<List<Sector>> ObtenerSectoresPorSeccionID(int seccionId)
+        {
+            var sectoresResult = sectores.FindAll(x => x.SeccionId == seccionId);
+            return await Task.FromResult(sectoresResult);
+        }
+
+        public async Task<List<Seccion>> ObtenerSeccionesPorDistritoMunicipalesID(int distritoMunicipalesId)
+        {
+            var seccionesResult = secciones.FindAll(x => x.DistritoMunicipalId == distritoMunicipalesId);
+            return await Task.FromResult(seccionesResult);
+        }
+
+        public async Task<List<DistritoMunicipal>> ObtenerDistritoMunicipalesPorMunicipioID(int municipioId)
+        {
+            var distritoMunicipalesResult = distritoMunicipales.FindAll(x => x.MunicipioId == municipioId);
+            return await Task.FromResult(distritoMunicipalesResult);
+        }
+
+        public async Task<List<Municipio>> ObtenerMunicipiosPorProvinciaID(int provinciaId)
+        {
+            var municipiosResult = municipios.FindAll(x => x.ProvinciaId == provinciaId);
+            return await Task.FromResult(municipiosResult);
+        }
+
+        public async Task<List<Provincia>> ObtenerProvinciasPorRegionID(int regionId)
+        {
+            var provinciasResult = provincias.FindAll(x => x.RegionId == regionId);
+            return await Task.FromResult(provinciasResult);
+        }
+
+        public async Task<List<Region>> ObtenerRegiones()
+        {
+           
+            return await Task.FromResult(regiones);
+        }
+
+        public async Task<List<Barrio>> ObtenerBarriosPorMunicipioID(int municipioId)
+        {
+            var distritoMunicipalesResult = distritoMunicipales.FindAll(x => x.MunicipioId == municipioId);
+            
+            foreach (var item in distritoMunicipalesResult)
+            {
+                var seccionesResult = ObtenerSeccionesPorDistritoMunicipalesID(item.DistritoMunicipalId);
+                foreach (var seccion in seccionesResult)
+                {
+
+                }
+            }
+
+
+            var barriosResult = barrios.FindAll(x => x.SectorId == sectorId);
+            return await Task.FromResult(barriosResult);
+        }
+
+
     }
 }
