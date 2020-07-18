a64ea1d5aea506620a2e99517507966e5264a92a
ErickIR <erickrc9827@gmail.com>
2020-07-18 16:02:08 -0400

    Changed the main color of status bar

--- Proyects/Xamarin/CitizenApp/CitizenApp/CitizenApp.Android/MainActivity.cs
+++ Proyects/Xamarin/CitizenApp/CitizenApp/CitizenApp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace CitizenApp.Droid
 {
-    [Activity(Label = "CitizenApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "CooperaRD", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
