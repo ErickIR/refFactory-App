621c58df7d816f47d8445a4dff5237be0146a696
ariel0798 <arielcruz07@hotmail.com>
2020-07-21 20:24:12 -0400

    Junta Central Simulador

---
+++ Proyects/JuntaSimulador/JuntaCentral/JC.Api/Context/JuntaDbContext.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace JC.Api.Context
+{
+    public partial class JuntaDbContext : DbContext
+    {
+        public JuntaDbContext()
+        {
+        }
+
+        public JuntaDbContext(DbContextOptions<JuntaDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Persona> Persona { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Persona>(entity =>
+            {
+                entity.HasKey(e => e.Cedula)
+                    .HasName("PK__Persona__B4ADFE392DFFDB13");
+
+                entity.Property(e => e.Cedula)
+                    .HasMaxLength(11)
+                    .IsUnicode(false)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Apellidos)
+                    .IsRequired()
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Barrio)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.DistritoMunicipal)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FechaNacimiento).HasColumnType("date");
+
+                entity.Property(e => e.Municipio)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nacionalidad)
+                    .IsRequired()
+                    .HasMaxLength(35)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Nombres)
+                    .IsRequired()
+                    .HasMaxLength(40)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Ocupacion)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Provincia)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Region)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Seccion)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Sector)
+                    .IsRequired()
+                    .HasMaxLength(20)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Sexo)
+                    .IsRequired()
+                    .HasMaxLength(1)
+                    .IsUnicode(false)
+                    .IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Proyects/JuntaSimulador/JuntaCentral/JC.Api/Controllers/PersonasController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using JC.Api.Context;
+
+namespace JC.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+        private readonly JuntaDbContext _context;
+
+        public PersonasController(JuntaDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Personas
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Persona>>> GetPersona()
+        {
+            return await _context.Persona.ToListAsync();
+        }
+
+        // GET: api/Personas/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Persona>> GetPersona(string id)
+        {
+            var persona = await _context.Persona.FindAsync(id);
+
+            if (persona == null)
+            {
+                return NotFound();
+            }
+
+            return persona;
+        }
+
+        // PUT: api/Personas/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPersona(string id, Persona persona)
+        {
+            if (id != persona.Cedula)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(persona).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Personas
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Persona>> PostPersona(Persona persona)
+        {
+            _context.Persona.Add(persona);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (PersonaExists(persona.Cedula))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetPersona", new { id = persona.Cedula }, persona);
+        }
+
+        // DELETE: api/Personas/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Persona>> DeletePersona(string id)
+        {
+            var persona = await _context.Persona.FindAsync(id);
+            if (persona == null)
+            {
+                return NotFound();
+            }
+
+            _context.Persona.Remove(persona);
+            await _context.SaveChangesAsync();
+
+            return persona;
+        }
+
+        private bool PersonaExists(string id)
+        {
+            return _context.Persona.Any(e => e.Cedula == id);
+        }
+    }
+}
